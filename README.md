065aa1cf8db0499a89deaf0a191f1c80a73932c8
Shelley Vohr <csvohr1995@gmail.com>
2017-07-28 09:17:50 -0700

    add support for both fialog types and improve README

--- README.md
+++ README.md
@@ -3,10 +3,18 @@
 This module allows for the opening of a native file window in KDE distributions, and is intended for use in Electron applications until a
 full fix has been created for [this](https://github.com/electron/electron/issues/2911) issue.
 
+Allows for triggering of two different kdialog windows:
+  * `OPEN` or `open`
+  * `SAVE` or `save`
+
 To use:
 
 ```javascript
-  const picker = require('native-kde-file-open');
+  const nativePicker = require('native-kde-file-open');
 
-  picker.openKDEFileWindow();
+  // to open single file or multiple files
+  nativePicker('OPEN');
+
+  // to save single file or multiple files
+  nativePicker('SAVE');
 ```
--- index.js
+++ index.js
@@ -3,12 +3,34 @@ const os = require('os');
 
 const platform = os.platform();
 
-function openKDEFileWindow() {
+const TYPES = [
+  'save',
+  'open'
+]
+
+module.exports = (type) => {
   if (platform === 'linux') {
-    exec('kdialog --getopenfilename .');
+    switch (type.toLowerCase()) {
+      case 'open':
+        exec('kdialog --getopenfilename .', (err, stdout, stderr) => {
+          console.log(`stdout: ${stdout}`);
+          if (err !== null) {
+            console.log(`exec error: ${err}`);
+          }
+        });
+        break;
+      case 'save':
+        exec('kdialog --getsavefilename .', (err, stdout, stderr) => {
+          console.log(`stdout: ${stdout}`);
+          if (err !== null) {
+            console.log(`exec error: ${err}`);
+          }
+        });
+        break;
+      default:
+        throw new Error('Invalid dialog box type specified.')
+    }
   } else {
     throw new Error('This is intended to be run on Linux KDE distributions.')
   }
 }
-
-module.exports = openKDEFileWindow;
